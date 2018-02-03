2ebaea28e9a0b9e1f3dc3c8ab3b1f6fe2a48b2c5
ThinhLa <thinhtrla@gmail.com>
2018-02-03 01:46:39 -0600

    Add project files.

---
+++ ISAM5635-MIT-Core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ISAM5635_MIT_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
